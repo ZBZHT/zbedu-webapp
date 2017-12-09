c0538d3242038798748184c538cd71c109188a24
xjxhub <904073612@qq.com>
2017-12-09 11:15:19 +0800

    修改views路径及index名称

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import index from '@/views/index'
+
 
 Vue.use(Router)
 
@@ -10,6 +12,11 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path:'/index',
+      name:'index',
+      component:index
     }
   ]
 })
