977211e740a3175ffc2c680203c4435a2b316851
xjxhub <904073612@qq.com>
2017-12-11 18:35:38 +0800

    添加新页面

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import index from '@/views/index'
+import textCenter from '@/views/textCenter'
 
 
 Vue.use(Router)
@@ -17,6 +18,11 @@ export default new Router({
       path:'/index',
       name:'index',
       component:index
+    },
+    {
+      path:'/textCenter',
+      name:'textCenter',
+      component:textCenter
     }
   ]
 })
