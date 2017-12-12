0864db1a0f4155238678b7c59dd498c2b134cc15
xiejiaxin <904073612@qq.com>
2017-12-12 20:47:06 +0800

    修改test、、、

---
+++ src/views/testQuestion.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="question">
+    
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+import swipe from '@/components/testCenter/swipe'
+
+
+export default {
+  name: 'textQuestion',
+  data () {
+    return {
+        
+    }
+  },
+  mounted(){
+      axios.get("/api/menu/index",{
+                params:{
+                     user:123
+                }
+            }).then((res)=>{
+                this.textCenterData = res.data;
+                console.log(this.textCenterData);
+
+            }).catch(function(error){
+                console.log("error init." + error)
+            });
+    },
+  components:{swipe}
+}
+</script>
+
+<style>
+.question{
+    min-width:960px;
+}
+</style>
