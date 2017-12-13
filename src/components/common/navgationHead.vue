55224fca028544e3236d50ab9f45cf12908abd42
xiejiaxin <904073612@qq.com>
2017-12-13 14:27:14 +0800

    新加head组件

---
+++ src/components/common/navgationHead.vue
@@ -0,0 +1,39 @@
+<template>
+    
+
+</template>
+
+<script>
+      import axios from 'axios'
+export default {
+  name: 'main',
+  props:['bannerLeftData'],
+  data () {
+    return {
+      msg: 'main'
+    }
+  },
+  mounted(){
+    
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+*{
+    margin:0;
+    padding:0;
+}
+ul li{
+    list-style: none;
+}
+a {
+    color: inherit;
+    cursor: pointer;
+}
+a:hover{
+    text-decoration: none;
+}
+
+</style>
