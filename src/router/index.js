24df0175e067a33e92b400366520887669111779
xiejiaxin <904073612@qq.com>
2017-12-13 11:19:43 +0800

    删除单个组件路由 保留主页面路由

--- src/router/index.js
+++ src/router/index.js
@@ -3,12 +3,6 @@ import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import index from '@/views/index'
 import testCenter from '@/views/testCenter'
-import header from '@/components/common/Header'
-import footer from '@/components/common/Footer'
-import dropmenu from '@/components/common/DropMenu'
-import login from '@/components/common/Login'
-
-
 
 Vue.use(Router)
 
@@ -28,26 +22,6 @@ export default new Router({
       path:'/testCenter',
       name:'testCenter',
       component:testCenter
-    },
-    {
-      path: '/header',
-      name: 'header',
-      component: header
-    },
-    {
-      path: '/footer',
-      name: 'footer',
-      component: footer
-    },
-    {
-      path: '/dropmenu',
-      name: dropmenu,
-      component: dropmenu
-    },
-    {
-      path: '/login',
-      name: 'login',
-      component: login
     }
   ]
 })
