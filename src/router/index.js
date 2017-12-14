7f71e96a5fefb0b8e78d4133ae33e7c96a7bb724
xiejiaxin <904073612@qq.com>
2017-12-14 17:59:22 +0800

    添加新路由testQuestion

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import index from '@/views/index'
 import testCenter from '@/views/testCenter'
+import testQuestion from '@/views/testQuestion'
 
 Vue.use(Router)
 
@@ -22,6 +23,11 @@ export default new Router({
       path:'/testCenter',
       name:'testCenter',
       component:testCenter
+    },
+    {
+      path:'/testQuestion',
+      name:'testQuestion',
+      component:testQuestion
     }
   ]
 })
