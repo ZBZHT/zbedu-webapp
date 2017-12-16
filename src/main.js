8e32fe8a957c484d0b5155be01f8a6866fc5fce7
xiejiaxin <904073612@qq.com>
2017-12-16 14:23:56 +0800

    修改router路由路径

--- src/main.js
+++ src/main.js
@@ -2,11 +2,8 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import router from './router'
-import VRouter from 'vue-router'
+import router from './router/index'
 
-
-Vue.use(VRouter)
 
 Vue.config.productionTip = false
 
