469644f40d0a4655026a952f47157c7f0fe2d91e
xurui <>
2017-12-22 17:59:22 +0800

    带树形的课程页

---
+++ src/views/CourseDetail.vue
@@ -0,0 +1,283 @@
+<template>
+  <div>
+    <div class="header-box">
+      <navgation-head></navgation-head>
+    </div>
+    <div class="content-box">
+      <div class="tree-box">
+        <div class="tree-menu">
+          <ul v-for="menuItem in theModel">
+            <my-tree :model="menuItem"></my-tree>
+          </ul>
+        </div>
+      </div>
+      <div class="right-box">
+        <p>子标题</p>
+        <div class="detail-box">
+          <ul class="nav-box">
+            <li v-for="(item,index) in detailNavData" @click="onclick(index)" :class="{line:line}">
+              <a href="#">{{item.title }}</a>
+            </li>
+          </ul>
+          <div class="course-box" v-show="this.currentIndex === 0">
+            <p class="introduce">动力电池盖拆解的步骤以及电动车高压的安全操作注意事项。工具的使用选择以及注意力的大小，密封胶的清洁</p>
+          </div>
+          <div class="appraise-box" v-show="this.currentIndex === 3">
+            <p>{{ appraiseMsg }}</p>
+            <textarea name="" id="" cols="30" rows="10">各种评价</textarea>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+  var myData = [
+      {
+        'id': '1',
+        'menuName': '一级--1',
+        'menuCode': '10',
+        'children': [
+          {
+            'menuName': '二级--1',
+            'menuCode': '11'
+          },
+          {
+            'menuName': '二级--2',
+            'menuCode': '12',
+            'children': [
+              {
+                'menuName': '三级--1',
+                'menuCode': '121'
+              },
+              {
+                'menuName': '三级--2',
+                'menuCode': '122'
+              },
+              {
+                'menuName': '三级--3',
+                'menuCode': '123'
+              },
+              {
+                'menuName': '三级--4',
+                'menuCode': '124'
+              },
+              {
+                'menuName': '三级--5',
+                'menuCode': '124'
+              }
+            ]
+          },
+          {
+            'menuName': '二级--3',
+            'menuCode': '13'
+          }
+        ]
+      },
+      {
+        'id': '2',
+        'menuName': '一级--2',
+        'menuCode': ''
+      },
+      {
+        'id': '3',
+        'menuName': '一级--3',
+        'menuCode': '30',
+        'children': [
+          {
+            'menuName': '二级--1',
+            'menuCode': '31'
+          },
+          {
+            'menuName': '二级--2',
+            'menuCode': '32',
+            'children': []
+          }
+        ]
+      },
+      {
+        'id': '4',
+        'menuName': '一级--4',
+        'menuCode': '',
+        'children': []
+      },
+    ]
+  import navgationHead from '@/components/common/navgationHead'
+  import myTree from '@/components/courseTree/tree'
+
+  export default {
+    name: 'user',
+    data () {
+      return {
+        currentIndex: 0,
+        appraiseMsg: '全部评价',
+        line: true,
+        msg: '',
+        // detailNavData:["课程详情","教学课件","教学微课","课程评价"
+        detailNavData: [
+          {
+            "title": "课程详情"
+          },
+          {
+            "title": "教学课件"
+          },
+          {
+            "title": "教学微课"
+          },
+          {
+            "title": "课程评价"
+          }
+        ],
+        theModel: myData
+      }
+    },
+    methods: {
+      onclick: function (index) {
+        this.currentIndex = index
+        if (this.currentIndex === 1) {
+          this.$router.push({name:'playPdf',params:'playPdf'})
+        }
+        if (this.currentIndex === 2) {
+          this.$router.push({path: '/playVideo'})
+        }
+      }
+    },
+    mounted(){
+    },
+    watch: {
+
+    },
+    components: {
+      navgationHead,myTree
+    }
+  }
+</script>
+
+<style scoped>
+  *{
+    margin: 0;
+    padding: 0;
+    list-style: none;
+  }
+  .nav{
+    background: #ddd;
+    position: absolute;
+    left: 0;
+    right: 0;
+    top: 0;
+  }
+  .content-box{
+    width: 100%;
+    min-width: 960px;
+    margin-top: 150px;
+    height: 700px;
+    background: lightgoldenrodyellow;
+    position: relative;
+  }
+  .content-box .tree-box{
+    width: 200px;
+    height: 500px;
+    background: lightcyan;
+    position: absolute;
+    left: 100px;
+  }
+  .content-box .right-box{
+    width: 800px;
+    height: 700px;
+    background: aliceblue;
+    position: absolute;
+    left: 350px;
+  }
+  .right-box p{
+    font-size: 20px;
+    font-weight: bold;
+    /*background: pink;*/
+    margin-bottom: 50px;
+  }
+   .right-box .detail-box{
+    height: 500px;
+    width: auto;
+    text-align: center;
+  }
+  .right-box .nav-box{
+    /*list-style: none;*/
+    height: 40px;
+    /*background: lightseagreen;*/
+    display: flex;
+    border-bottom: 1px solid #333;
+  }
+  .right-box .nav-box li{
+    /*height: 40px;*/
+    line-height: 40px;
+    width: 100px;
+    background: linen;
+    border: 1px solid #444;
+    border-radius: 5px 5px 0px 0;
+    margin-left: 10px;
+    text-align: center;
+  }
+  .right-box .nav-box li{
+    border-bottom: none;
+  }
+  .right-box .nav-box .line{
+    border-bottom: 1px solid red;
+  }
+  .right-box .nav-box li a{
+    text-decoration: none;
+    color: #000;
+  }
+  .right-box .nav-box li a:hover{
+    color: red;
+  }
+  .right-box .nav-box li:first-child{
+    margin-left: 190px;
+  }
+  .right-box .course-box{
+    width: 600px;
+    height: 400px;
+    margin-left: 100px;
+    position: relative;
+    /*background: lavender;*/
+    background: url("../assets/bbb.png") no-repeat;
+    padding: 130px 100px;
+  }
+
+  .right-box .course-box p{
+    width: 500px;
+    font-size: 16px;
+    font-weight: normal;
+    /*background: red;*/
+    word-wrap: break-word;
+  }
+  .right-box .appraise-box{
+    width: 700px;
+    height: 400px;
+    margin-left: 50px;
+    position: relative;
+    top: 20px;
+    background: lightyellow;
+  }
+  .right-box .appraise-box p{
+    font-weight: normal;
+    font-size: 16px;
+    text-align: left;
+    padding: 10px;
+    /*background: red;*/
+    border-bottom: 1px solid #ccc;
+  }
+  .right-box .appraise-box textarea{
+    width: 500px;
+    height: 300px;
+    autofocus: autofocus;
+    overflow: hidden;
+  }
+  .tree-menu{
+    background: pink;
+  }
+
+  /*.noneLine{*/
+      /*border-bottom: 1px solid #fff;*/
+    /*}*/
+</style>
