1765b6b63bf7b9d2962913a4b23522ef50a62767
xurui <>
2017-12-22 18:18:16 +0800

    配置路由

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,10 @@ import HelloWorld from '@/components/HelloWorld'
 import index from '@/views/index'
 import testCenter from '@/views/testCenter'
 import testQuestion from '@/views/testQuestion'
+import course from '@/views/CourseDetail'
+import courseNoTree from '@/views/CourseDetailNoTree'
+import playPdf from '@/views/playPdf'
+import playVideo from '@/views/playVideo'
 
 Vue.use(Router)
 
@@ -28,6 +32,26 @@ export default new Router({
       path:'/testQuestion',
       name:'testQuestion',
       component:testQuestion
+    },
+    {
+      path: '/course',
+      name: 'course',
+      component: course
+    },
+    {
+      path: '/courseNoTree',
+      name: 'courseNoTree',
+      component: courseNoTree
+    },
+    {
+      path: '/playPdf',
+      name: 'playPdf',
+      component: playPdf
+    },
+    {
+      path: '/playVideo',
+      name: 'playVideo',
+      component: playVideo
     }
   ]
 })
