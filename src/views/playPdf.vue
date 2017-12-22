2ab4aa45c8a80b4f0599a92049f639a03a288a9f
xurui <>
2017-12-22 18:02:25 +0800

    pdf播放页

---
+++ src/views/playPdf.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <div class="header-box">
+      <navgation-head></navgation-head>
+    </div>
+    <div class="content-box">
+      <iframe src="/src/assets/pdf/EV系列-2-3-电机继电器故障检测.pdf" class="pdf-box"></iframe>
+    </div>
+
+  </div>
+</template>
+
+<script>
+  import navgationHead from '@/components/common/navgationHead'
+  export default {
+    name: 'user',
+    data () {
+      return {
+
+      }
+    },
+    mounted(){
+    },
+    watch: {
+
+    },
+    components: {
+      navgationHead
+    }
+  }
+</script>
+
+<style scoped>
+  *{
+    margin: 0;
+    padding: 0;
+    list-style: none;
+  }
+  .nav{
+    background: #ddd;
+    position: absolute;
+    left: 0;
+    right: 0;
+    top: 0;
+  }
+  .content-box{
+    width: 100%;
+    min-width: 960px;
+    margin-top: 105px;
+    height: 700px;
+    /*background: lightgoldenrodyellow;*/
+    position: relative;
+    padding: 0 5%;
+  }
+  .content-box .pdf-box{
+    min-width: 1000px;
+    width: 90%;
+    height: 100%;
+  }
+
+</style>
