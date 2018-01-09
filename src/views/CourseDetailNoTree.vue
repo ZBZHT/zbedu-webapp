b3fbe3f3624c2a2bf635465189233379d7bf4fd1
xurui <>
2018-01-09 18:05:10 +0800

    写评论功能

--- src/views/CourseDetailNoTree.vue
+++ src/views/CourseDetailNoTree.vue
@@ -16,8 +16,93 @@
             <p class="introduce">{{ currentCourseData.describe }}</p>
           </div>
           <div class="appraise-box" v-show="this.currentIndex === 3">
-            <p>{{ appraiseMsg }}</p>
-            <textarea name="" id="" cols="30" rows="10">各种评价</textarea>
+            <p class="appraiseTitle">{{ appraiseMsg }}</p>
+            <div class="text">
+              <div v-for="(name,index) in arr" >
+                <p class="comment">{{name.text}}</p>
+                <p class="all">
+
+                  <input
+                    type="radio"
+                    name="nameType"
+                    value="0" v-model="score" checked="" disabled="disabled"/>
+                  <span>★</span>
+                  <input
+                    type="radio"
+                    name="nameType"
+                    value="1" v-model="score" checked="" disabled="disabled"/>
+                  <span>★</span>
+                  <input
+                    type="radio"
+                    name="nameType"
+                    value="2" v-model="score" checked="" disabled="disabled"/>
+                  <span>★</span>
+                  <input
+                    type="radio"
+                    name="nameType"
+                    value="3" v-model="score" checked="" disabled="disabled"/>
+                  <span>★</span>
+                  <input
+                    type="radio"
+                    name="nameType"
+                    value="4" v-model="score" checked="" disabled="disabled"/>
+                  <span>★</span>
+                  <input
+                    type="radio"
+                    name="nameType"
+                    value="5" v-model="score" checked="" disabled="disabled"/>
+                  <span>★</span>
+
+                </p>
+                <span>{{ arrData[score-1]}}</span>
+                <span v-on:click="dele(index)">❎</span>
+                <p class="time-box">时间：{{ currentdate }}</p>
+
+                <hr>
+              </div>
+
+            </div>
+            <textarea type="text" v-model="text"/>
+            <button @click="submitComments">提交评论</button>
+            <div id="shoplist">
+              <span>请评价：</span>
+              <p class="all">
+
+                <input
+                  type="radio"
+                  name="b"
+                  value="0" v-model="inputdata"/>
+                <span>★</span>
+                <input
+                  type="radio"
+                  name="b"
+                  value="1" v-model="inputdata" />
+                <span>★</span>
+                <input
+                  type="radio"
+                  name="b"
+                  value="2" v-model="inputdata" />
+                <span>★</span>
+                <input
+                  type="radio"
+                  name="b"
+                  value="3" v-model="inputdata" />
+                <span>★</span>
+                <input
+                  type="radio"
+                  name="b"
+                  value="4" v-model="inputdata" />
+                <span>★</span>
+                <input
+                  type="radio"
+                  name="b"
+                  value="5" v-model="inputdata" />
+                <span>★</span>
+
+              </p>
+              <span>{{ arrData[inputdata-1]}}</span>
+            </div>
+            <!--<textarea name="" id="" cols="30" rows="10">各种评价</textarea>-->
           </div>
         </div>
       </div>
@@ -29,7 +114,7 @@
 
   import navgationHead from '@/components/common/navgationHead'
   import bus from '../assets/js/Bus'
-
+  var i=0;
   var busData = {
 
   };
@@ -59,7 +144,14 @@
           {
             "title": "课程评价"
           }
-        ]
+        ],
+        text: '',
+        arr: [],
+        inputdata: 0,
+        arrData: ['一星', '两星', '三星', '四星', '五星'],
+        currentdate: '',
+        nanmeType: '',
+        score: 0
       }
     },
     methods: {
@@ -71,12 +163,41 @@
         if (this.currentIndex === 2) {
           this.$router.push({path: '/playVideo'})
         }
-      }
+      },
+      dele: function (index) {
+        this.arr.splice(index, 1)
+      },
+      submitComments () {
+        if (this.text === '') {
+          alert('评论不能为空')
+        } else {
+          this.arr.push({text: this.text})
+          this.text = ''
+          console.log(this.arr)
+        }
+        var date = new Date()
+        var seperator1 = '-'
+        var seperator2 = ':'
+        var month = date.getMonth() + 1
+        var strDate = date.getDate()
+        if (month >= 1 && month <= 9) {
+          month = '0' + month
+        }
+        if (strDate >= 0 && strDate <= 9) {
+          strDate = '0' + strDate
+        }
+        this.currentdate = date.getFullYear() + seperator1 + month + seperator1 + strDate
+          + ' ' + date.getHours() + seperator2 + date.getMinutes()
+          + seperator2 + date.getSeconds()
+        this.nameType = ++i
+        this.score = this.inputdata
+        // alert(this.currentdate)
     },
-    mounted(){
     },
     watch: {
-
+      inputdata (value) {
+        console.log(value)
+      }
     },
     components: {
       navgationHead
@@ -187,7 +308,7 @@
     top: 20px;
     background: lightyellow;
   }
-  .right-box .appraise-box p{
+  .right-box .appraise-box .appraiseTitle{
     font-weight: normal;
     font-size: 16px;
     text-align: left;
@@ -196,15 +317,63 @@
     /*background: red;*/
     border-bottom: 1px solid #ccc;
   }
-  .right-box .appraise-box textarea{
-    width: 600px;
-    height: 300px;
-    autofocus: autofocus;
-    overflow: hidden;
-  }
+  /*.right-box .appraise-box textarea{*/
+    /*width: 600px;*/
+    /*height: 300px;*/
+    /*autofocus: autofocus;*/
+    /*overflow: hidden;*/
+  /*}*/
   .tree-menu{
     background: pink;
   }
+
+  .appraise-box textarea{
+    background: deeppink;
+    height: 60px;
+    width: 80%;
+    font-size: 20px;
+  }
+  .appraise-box .text{
+    background: lightsalmon;
+  }
+  .appraise-box .text .comment{
+    font-size: 20px;
+    background: palegreen;
+  }
+  .appraise-box .text .all{
+    height: 40px;
+    background: white;
+    display: inline-block;
+  }
+  .appraise-box .text .time-box{
+    text-align: left;
+    font-size: 20px;
+    font-weight: normal;
+    margin-left: 50px;
+    background: lightseagreen;
+  }
+
+  .appraise-box .all>input{opacity:0;position:absolute;width:2em;height:2em;margin:0;}
+
+  .appraise-box .all>input:nth-of-type(1),
+
+  .appraise-box .all>span:nth-of-type(1){display:none;}
+
+  .appraise-box .all>span{font-size:1em;color:gold;
+
+    -webkit-transition:color
+    .2s;
+
+    transition:color
+    .2s;
+  }
+  .appraise-box .all>input:checked~span{color:#666;}
+
+  .appraise-box .all>input:checked+span{color:gold;}
+
+
+
+
 
   /*.noneLine{*/
   /*border-bottom: 1px solid #fff;*/
