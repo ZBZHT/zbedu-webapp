1d9f4e4b8e6242c7f0e7737faa9a6003caa442ed
xurui <>
2018-01-15 17:46:22 +0800

    调整树形结构样式

--- src/components/courseTree/tree.vue
+++ src/components/courseTree/tree.vue
@@ -1,23 +1,20 @@
 <template>
-<div>
-  <li class="listItem">
-    <span @click="toggle">
+  <li class="listItem" @mouseenter="enter">
+    <span @click="toggle" >
      <i v-if="isInclude" class="icon" :class="[open ? 'folder-open': 'folder']"></i>
      <i v-if="!isInclude" class="icon"></i>
-     <a href="#" >{{course.title}}</a>
+     <a href="#">{{course.title}}</a>
     </span>
     <ul class="son-box" v-show="open">
-      <li v-for="item in course.children" v-if="item" class="sonListItem">
-        <span @click="toggle1">
-         <i v-if="item" class="icon" :class="[open1 ? 'folder-open': 'folder']"></i>
-         <i v-if="!isInclude" class="icon"></i>
-
+      <li v-for="(item,index) in course.children" v-if="item" class="sonListItem" @mouseenter="enter1(index)" @mouseleave="leave">
+        <span @click="toggle1(index)" :class="{'addBgcolor': isChange && (index === currentIndex)}">
+         <i v-if="item.children" class="icon" :class="[(open1 && currentOpenIndex === index) ? 'folder-open': 'folder']"></i>
+         <!--<i v-if="!isInclude" class="icon"></i>-->
          <a href="#" @click="sendMsg(item)">{{item.title}}</a>
-
         </span>
-        <ul class="grandson-box" v-show="open1" v-if="item">
-          <li class="grandsonListItem" v-for="item2 in item.children">
-            <span>
+        <ul class="grandson-box" v-show="(open1 && currentOpenIndex === index)" v-if="item">
+          <li class="grandsonListItem" v-for="(item2,index2) in item.children" @mouseenter="enter2(index2)" @mouseleave="leave">
+            <span :class="{'addBgcolor': isChange && (index2 === currentIndex1)}">
              <i v-if="item && item2"></i>
              <a href="#" @click="sendMsg(item2)">{{item2.title}}</a>
             </span>
@@ -26,11 +23,9 @@
       </li>
     </ul>
   </li>
-</div>
 </template>
 <script>
   import Vue from 'vue'
-
   export default {
     name: 'tree',
     data() {
@@ -38,11 +33,15 @@
         open: true,
         isFolder: true,
         open1: false,
-        isInclude:true
-              }
+        isInclude:true,
+        isChange:false,
+        currentIndex:-1,
+        currentIndex1:-1,
+        currentOpenIndex:-1
+      }
     },
     computed:{
-      
+
       course(){
           return this.$store.state.course;
       }
@@ -56,10 +55,30 @@
       toggle1: function(index) {
         if (this.isInclude) {
           this.open1 = !this.open1
+          this.currentOpenIndex = index
         }
+
+        // alert(index)
+        // alert(this.open1)
       },
       sendMsg(item){
         this.$store.commit('noTreeTitle',item);
+      },
+      enter1(index) {
+        this.isChange = true
+        this.currentIndex  = index
+        // alert(index)
+      },
+      enter2(index) {
+        this.isChange = true
+        this.currentIndex1  = index
+        // alert(index)
+      },
+      leave(){
+        this.currentIndex = -1
+      },
+      enter(){
+        this.isChange = true
       }
 
     },
@@ -69,39 +88,43 @@
   }
 </script>
 <style scoped>
+  ul{
+    list-style: none;
+    margin: 0;
+    padding: 0;
+  }
   a{
     color: #fff;
     text-decoration: none;
   }
-  ul {
-    list-style: none;
-    text-align: left;
-    overflow: hidden;
-
-  }
   .listItem{
     background: #000;
-    width: 250px;
+    /*width: 250px;*/
     line-height: 40px;
     color: #fff;
     margin-left: 10px;
   }
-  .listItem li{
+  .listItem span{
+    display: inline-block;
+    /*line-height: 40px;*/
+    width: 270px;
+    text-align: left;
+    /*background: pink;*/
+  }
+  .listItem .sonListItem{
     border-bottom: 1px solid #fff;
     line-height: 40px;
     margin-top: 0px;
     text-overflow: ellipsis;
-    overflow: hidden;
     white-space: nowrap;
-
+    /*background: lightseagreen;*/
   }
-  .listItem a:hover{
+  .listItem  .addBgcolor{
     background: red;
-    color: #fff;
   }
   .listItem a{
     display: inline-block;
-    width: 230px;
+    /*width: 230px;*/
     line-height: 40px;
   }
   i.icon {
@@ -122,18 +145,12 @@
     background-image: url(/src/assets/mnus.png);
   }
   .son-box{
-    margin-left: 0px;
+    margin: 0px;
     background: #999;
-    overflow: hidden;
-  }
-  .son-box .sonListItem{
-    /*border-bottom: 1px solid #fff;*/
-    /*line-height: 40px;*/
   }
   .grandson-box{
     margin-left: 0px;
     background: #ccc;
-    overflow: hidden;
   }
   .grandson-box .grandsonListItem{
     border-bottom: 1px solid #fff;
