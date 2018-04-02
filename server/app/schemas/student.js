4a38175ef155d2968344e093119a8f6095079381
xiejiaxin <904073612@qq.com>
2018-04-02 14:05:33 +0800

    server

---
+++ server/app/schemas/student.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+//用户表结构
+module.exports = new Schema({
+  n_name: {
+    type: String,
+  },
+  user: {
+    type: String,
+    required: true, //设定是否必填
+  },
+  pwd: {
+    type: String,
+  },
+  userID: {
+    type: String,
+    unique: true,  //索引值唯一
+    required: true, //设定是否必填
+  },
+  IDNo: {
+    type: String,
+    unique: true,  //索引值唯一
+    required: true, //设定是否必填
+  },
+  MoNo: {
+    type: String,
+    unique: true,  //索引值唯一
+    required: true, //设定是否必填
+  },
+  userType: {
+    type: String,
+    required: true, //设定是否必填
+  },
+  gender: {
+    type: Number,
+    required: true, //设定是否必填
+  },
+  AdmDate: {  //入学时间
+    type: Date,
+    required: true, //设定是否必填
+  },
+  major: {  //专业
+    type: String,
+    required: true, //设定是否必填
+  },
+  classGrade: {  //班级
+    type: String,
+    required: true, //设定是否必填
+  }
+});
