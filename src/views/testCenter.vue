9ff57cde25e30a6b11747711eea4581e0a981668
xiejiaxin <904073612@qq.com>
2019-01-02 16:22:12 +0800

    上传测试

--- src/views/testCenter.vue
+++ src/views/testCenter.vue
@@ -6,7 +6,7 @@
     </canvas>
   </div>
 
-  <div @click="appFullScreen">124578</div>
+  <!--<div @click="appFullScreen">124578</div>-->
   </div>
 </template>
 
@@ -22,12 +22,10 @@ export default {
     return {
       balls:[],
       circles:[
-        {x:64,y:35,color:'red'},
-        {x:145,y:99,color:'black'},
-        {x:241,y:165,color:'red'},
-        {x:333,y:230,color:'green'},
-        {x:423,y:285,color:'black'}
-      ]
+        {x:343.5,y:276.5,a:[340.5,273.5],b:[346.5,273.5],c:[346.5,279.5],d:[340.5,279.5],color:'red'},
+        {x:353.5,y:276.5,a:[350.5,273.5],b:[356.5,273.5],c:[356.5,279.5],d:[350.5,279.5],color:'black'},
+        {x:353.5,y:286.5,a:[350.5,283.5],b:[356.5,283.5],c:[356.5,289.5],d:[350.5,289.5],color:'red'}
+      ],
     }
   },
   created(){
@@ -55,18 +53,27 @@ export default {
       this.ctx.clearRect(0,0,this.canvas.width,this.canvas.height);
       for( var i = 0 ; i < this.balls.length ; i ++ ){
         this.ctx.beginPath();
-        this.ctx.arc(this.balls[i].x,this.balls[i].y,this.balls[i].radius,0,2*Math.PI);
+//        this.ctx.arc(this.balls[i].x,this.balls[i].y,this.balls[i].radius,0,2*Math.PI);
+        this.ctx.moveTo(this.balls[i].a[0],this.balls[i].a[1])
+        this.ctx.lineTo(this.balls[i].b[0],this.balls[i].b[1])
+        this.ctx.lineTo(this.balls[i].c[0],this.balls[i].c[1])
+        this.ctx.lineTo(this.balls[i].d[0],this.balls[i].d[1])
         if(this.ctx.isPointInPath(x,y)){//isPointInPath() 方法返回 true，如果指定的点位于当前路径中；否则返回 false。
           console.log('yes',this.balls[i].x,this.balls[i].y)
 //          this.ctx.clearRect(0, 0, 100, 100);
-          this.ctx.arc(this.balls[i].x,this.balls[i].y,this.balls[i].radius+2,0,2*Math.PI)
+//          this.ctx.arc(this.balls[i].x,this.balls[i].y,this.balls[i].radius+2,0,2*Math.PI)
+          this.ctx.moveTo(this.balls[i].a[0]-1,this.balls[i].a[1]-1)
+          this.ctx.lineTo(this.balls[i].b[0]+1,this.balls[i].b[1]-1)
+          this.ctx.lineTo(this.balls[i].c[0]+1,this.balls[i].c[1]+1)
+          this.ctx.lineTo(this.balls[i].d[0]-1,this.balls[i].d[1]+1)
+          this.ctx.closePath()
+          this.ctx.stroke();
         }else{
 //          console.log('bai')
         }
-        this.ctx.fillStyle=this.balls[i].color
-//        this.ctx.closePath()
-        this.ctx.fill();
-
+//        this.ctx.fillStyle=this.balls[i].color
+        this.ctx.closePath()
+        this.ctx.stroke();
       }
     },
     first() {
@@ -78,6 +85,10 @@ export default {
           radius:radius,
           x:this.circles[i].x,
           y:this.circles[i].y,
+          a:[this.circles[i].a[0],this.circles[i].a[1]],
+          b:[this.circles[i].b[0],this.circles[i].b[1]],
+          c:[this.circles[i].c[0],this.circles[i].c[1]],
+          d:[this.circles[i].d[0],this.circles[i].d[1]],
           color:this.circles[i].color
         }
         this.balls[i]=this.aBall;
